352a145d5a6620a503f893c4b96d97a3f6e5f1e2
Luzian Zagadinow <luzianz@outlook.com>
2018-02-02 13:07:51 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Demo.Configuration {
+
+	class Program {
+
+		static void Main(string[] args) {
+			var builder = new ConfigurationBuilder();
+			builder.SetBasePath(Directory.GetCurrentDirectory());
+			builder.AddJsonFile("appsettings.json");
+
+			var configuration = builder.Build();
+			Console.WriteLine(configuration["A:B:C"]);
+		}
+	}
+}
